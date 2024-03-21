7d43f16383597b2b9f5718b22cd29a624668e8f5
samtabareh <sam.tabareh@gmail.com>
2024-03-21 17:35:13 +0330

    V1.4

--- Main.gd
+++ Main.gd
@@ -8,6 +8,8 @@ var Extensions := []
 var open_file_folder : bool
 var open_config : bool
 
+const FKEY = "!"
+
 func _ready():
 	load_config()
 
@@ -16,20 +18,12 @@ func _on_dir_selected(path: String):
 	print_rich("[color=yellow]Selected folder: [/color][color=green]%s[/color]" %path)
 	
 	var dir = DirAccess.open(path)
-	dir = open_dir(dir)
-	
-	if Files.is_empty():
-		print_rich("[color=red]No files with appropriate extension found. Maybe you should add your extensions in the config file[/color]")
-		return
-	
-	var file = Files.pick_random()
-	
-	print_rich("[color=yellow]Opening file: [/color][color=purple]%s[/color]" %file)
-	
-	OS.shell_open(file)
-	if open_file_folder: OS.shell_show_in_file_manager(file)
+	dir =  await open_dir(dir)
+	$VBoxContainer/Open.disabled = false
+	print_rich("[color=yellow]Files: [/color][color=orange]%s[/color]"% Files.size())
 
 func open_dir(dir : DirAccess):
+	$VBoxContainer/Open.disabled = true
 	if !dir: print_rich("[color=red]An error occurred when trying to access the path.
 	Error: "+str(DirAccess.get_open_error())+ "[/color]")
 	
@@ -43,7 +37,8 @@ func open_dir(dir : DirAccess):
 			print_rich("[color=cyan]%s[/color]" % file_name)
 			open_dir(DirAccess.open(path+"/"+file_name))
 		else:
-			if Extensions.has(file_name.get_extension()):
+			if (!Extensions.has(FKEY+file_name.get_extension()) 
+			&& Extensions.has(file_name.get_extension())) || Extensions.has("any"):
 				Files.append(path+"/"+file_name)
 		file_name = dir.get_next()
 
@@ -51,9 +46,9 @@ func load_config():
 	var config = ConfigFile.new()
 	
 	if !FileAccess.file_exists(Config_Path):
-		config.set_value("Random Chooser", "extensions", ["mp4", "mp3", "mkv"])
-		config.set_value("Random Chooser", "open_file_folder", true)
-		config.set_value("Random Chooser", "open_config", false)
+		config.set_value("Random Chooser", "extensions", ["mp4", "mkv", "!mp3"])
+		config.set_value("Random Chooser", "open_file_folder", false)
+		config.set_value("Random Chooser", "open_config", true)
 		config.save(Config_Path)
 		print_rich("[color=yellow]Made config file[/color]")
 	
@@ -68,13 +63,25 @@ func load_config():
 			open_file_folder = config.get_value(section, key)
 		if key == "open_config":
 			open_config = config.get_value(section, key)
-			$Config.text = "Open Config" if open_config else "Open Config Folder"
+			$VBoxContainer/Config.text = "Open Config" if open_config else "Open Config Folder"
 	
 	print_rich("[color=yellow]Loaded config file[/color]")
 
 func _on_select_pressed():
 	print_rich("[color=yellow]Selecting folder...[/color]")
-	$Select/NativeFileDialog.show()
+	$VBoxContainer/Select/NativeFileDialog.show()
+
+func _on_open_pressed():
+	if Files.is_empty():
+		print_rich("[color=red]No files with appropriate extension found. Maybe you should add your extensions in the config file[/color]")
+		return
+	
+	var file = Files.pick_random()
+	
+	print_rich("[color=yellow]Opening file: [/color][color=purple]%s[/color]" %file)
+	
+	OS.shell_open(file)
+	if open_file_folder: OS.shell_show_in_file_manager(file)
 
 func _on_config_pressed():
 	if !open_config:
