bdf1a8d8d30fee572c617ffbd1072258b02ddc1e
samtabareh <sam.tabareh@gmail.com>
2024-03-22 09:41:13 +0330

    V1.4.1

--- Main.gd
+++ Main.gd
@@ -2,8 +2,10 @@ extends CanvasLayer
 
 var Config_Path = "user://config.ini"
 var Global_Config_Path = ProjectSettings.globalize_path(Config_Path)
+var Selected_Path : String
 var Files := []
 
+var old_extensions := []
 var Extensions := []
 var open_file_folder : bool
 var open_config : bool
@@ -15,11 +17,11 @@ func _ready():
 
 func _on_dir_selected(path: String):
 	Files = []
+	Selected_Path = path
 	print_rich("[color=yellow]Selected folder: [/color][color=green]%s[/color]" %path)
 	
 	var dir = DirAccess.open(path)
 	dir =  await open_dir(dir)
-	$VBoxContainer/Open.disabled = false
 	print_rich("[color=yellow]Files: [/color][color=orange]%s[/color]"% Files.size())
 
 func open_dir(dir : DirAccess):
@@ -37,10 +39,11 @@ func open_dir(dir : DirAccess):
 			print_rich("[color=cyan]%s[/color]" % file_name)
 			open_dir(DirAccess.open(path+"/"+file_name))
 		else:
-			if (!Extensions.has(FKEY+file_name.get_extension()) 
-			&& Extensions.has(file_name.get_extension())) || Extensions.has("any"):
+			if Extensions.has(FKEY+file_name.get_extension()): return
+			elif Extensions.has(file_name.get_extension()) || Extensions.has("any"):
 				Files.append(path+"/"+file_name)
 		file_name = dir.get_next()
+	$VBoxContainer/Open.disabled = false
 
 func load_config():
 	var config = ConfigFile.new()
@@ -58,12 +61,16 @@ func load_config():
 	
 	for section in config.get_sections(): for key in config.get_section_keys(section):
 		if key == "extensions":
+			old_extensions = Extensions
 			Extensions = config.get_value(section, key)
 		if key == "open_file_folder":
 			open_file_folder = config.get_value(section, key)
 		if key == "open_config":
 			open_config = config.get_value(section, key)
 			$VBoxContainer/Config.text = "Open Config" if open_config else "Open Config Folder"
+	
+	for ex in Extensions: if !old_extensions.has(ex) && Selected_Path:
+		_on_dir_selected(Selected_Path)
 	
 	print_rich("[color=yellow]Loaded config file[/color]")
 
@@ -77,6 +84,10 @@ func _on_open_pressed():
 		return
 	
 	var file = Files.pick_random()
+	if Extensions.has("!"+file.get_extension()):
+		print_rich("[color=red]Selected file had a filtered extension. [b]Please Try Again[/b][/color]")
+		Files.erase(file)
+		return
 	
 	print_rich("[color=yellow]Opening file: [/color][color=purple]%s[/color]" %file)
 	
